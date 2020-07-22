9d03aa24fed23df0bd006fc58c1b4c7f8e74c19b
QQZE <qqze@thinkpad.home>
2020-07-22 15:07:33 +0100

    Finished sell.

--- stock.py
+++ stock.py
@@ -22,8 +22,6 @@ def buy():
     global portfolio
     symbol = input("Enter stock symbol: ")
     url = "https://uk.finance.yahoo.com/quote/" + symbol
-    #url = "https://uk.finance.yahoo.com/quote/AAPL?p=AAPL&.tsrc=fin-srch"
-    print(url)
     headers = {"User-Agent" : "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) snap Chromium/83.0.4103.61 Chrome/83.0.4103.61 Safari/537.36"}
     request = requests.get(url, headers=headers)
     soup = BeautifulSoup(request.content, 'html.parser')
@@ -52,6 +50,52 @@ def buy():
         portfolio.append([symbol,price,amount])
         funds -= totalsum
         print("Successfully purchased stock")
+
+def sell():
+    global funds
+    global portfolio
+    try:
+        symbol = input("Enter stock symbol to sell: ")
+        names = [x[0] for x in portfolio]
+        index = names.index(symbol)
+    except:
+        print(f"ERROR - no {symbol} stock is owned")
+        return
+    print(f"Amount owned: {portfolio[index][2]}")
+    try:
+        amount = int(input("Input amount of stocks to sell: "))
+    except ValueError:
+        print("\nERROR - incorrect data type")
+        return
+    if amount > portfolio[index][2]:
+        print("ERROR - invalid input")
+        return
+    url = "https://uk.finance.yahoo.com/quote/" + symbol
+    headers = {"User-Agent" : "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) snap Chromium/83.0.4103.61 Chrome/83.0.4103.61 Safari/537.36"}
+    request = requests.get(url, headers=headers)
+    soup = BeautifulSoup(request.content, 'html.parser')
+    price = float(soup.find("span", class_="Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)").get_text())
+    print(f"Stock bought at: ${portfolio[index][1]}")
+    print(f"Current stock price: ${price}")
+    print(f"Profit/loss: ${float(portfolio[index][1]) - float(price)}\n")
+    sold = input(f"Would you like to sell {symbol} stock at ${price} (type Y or N): ")
+    if sold.lower() == "n":
+        print("Request cancelled")
+        return
+    elif sold.lower() == "y":
+        pass
+    else:
+        print("ERROR - invalid input")
+        return
+    amountnew = portfolio[index][2] - amount
+    if amountnew == 0:
+        del portfolio[index]
+    else:
+        portfolio[index][2] = amountnew
+    funds += float(portfolio[index][1]) - float(price)
+    print(f"Successfully sold {symbol} stock at ${price}, your funds available are ${funds}")
+    if funds < 0:
+        print("\nFunds available have reached less than 0, please type !START to reset")
 
 def fund():
     print(f"Current funds available: {funds}")
@@ -88,7 +132,7 @@ def commands():
 !STOCKS - displays the currently owned stocks\n #
     """)
 
-globals = {'!BUY' : buy, '!START' : start, '!QUIT' : quit, '!COMMANDS' : commands, '!STOCKS' : stocks, '!FUND' : fund}
+globals = {'!BUY' : buy, '!START' : start, '!QUIT' : quit, '!COMMANDS' : commands, '!STOCKS' : stocks, '!FUND' : fund, '!SELL' : sell}
 
 while True:
     inp = input("Enter command: ")
