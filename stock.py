4c85048de3052ec0662c5673887838e986728d98
QQZE <qqze@thinkpad.home>
2020-07-22 11:58:43 +0100

    Added some extra stuff with buy

--- stock.py
+++ stock.py
@@ -1,42 +1,73 @@
 #Python stock market simulator
+fileread = open("data.txt", "r")
 from globals import *
+from bs4 import BeautifulSoup
+import requests
 
+funds = fileread.readline()
+portfolio = fileread.readline().strip().split(",")
+for x in range(len(portfolio)):
+    portfolio[x] = portfolio[x].split("-")
+    portfolio[x][1] = float(portfolio[x][1])
+    portfolio[x][2] = int(portfolio[x][2])
+print(portfolio)
 
 print(f"""\nThis is a real time investment simulation. \n
-If you are new or want to reset the simulation, \ntype !START, otherwhise type !CONTINUE. \n
+If you are new or want to reset the simulation, type !START. \n
 To see a list of commands, type !COMMANDS {line}""")
 
-def start():
-        fundstart = input("Enter your starting amount(GBP): ")
-        print("Unknown error")
-    if not type(fundstart) is int:
+#FUNCTIONS
+
+def buy():
+    symbol = input("Enter stock symbol: ")
+    url = "https://uk.finance.yahoo.com/quote/" + symbol
+    #url = "https://uk.finance.yahoo.com/quote/AAPL?p=AAPL&.tsrc=fin-srch"
+    print(url)
+    headers = {"User-Agent" : "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) snap Chromium/83.0.4103.61 Chrome/83.0.4103.61 Safari/537.36"}
+    request = requests.get(url, headers=headers)
+    soup = BeautifulSoup(request.content, 'html.parser')
+    try:
+        price = float(soup.find("span", class_="Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)").get_text())
+    except:
+        print("ERROR - invalid stock symbol")
         return
+    print(type(price))
+    print(price)
+
+
+def start():
+    funds = input("Enter your starting amount(GBP): ")
+    try:
+        float(funds)
+    except ValueError:
+        print("\nERROR - incorrect data type")
+        return
+    print("\nSuccessfully set funds")
+    portfolio = []
 
 def quit():
     exit()
 
 def commands():
     print("""
-!START - clears data and prompts user to enter starting funds amount\n
-!QUIT - stops the process and closes the application\n
-!BUY - displays menu to buy stocks\n
-!SELL - displays menu to sell your current stocks\n
-!STOCKS - displays the currently owned stocks\n
-!PRICE {stock symbol} - displays live price of stock\n
-!FUNDS - displays the current funds available\n
 !ABOUT - displays information about the program and creator\n
+!BUY - displays menu to buy stocks\n
+!FUNDS - displays the current funds available\n
+!PRICE {stock symbol} - displays live price of stock\n
+!QUIT - stops the process and closes the application\n
+!SAVE - saves current stocks and available funds\n
+!SELL - displays menu to sell your current stocks\n
+!START - clears data and prompts user to enter starting funds amount\n
+!STOCKS - displays the currently owned stocks\n
     """)
 
-globals = {'!START' : start, '!QUIT' : quit, '!COMMANDS' : commands}
+globals = {'!BUY' : buy, '!START' : start, '!QUIT' : quit, '!COMMANDS' : commands}
 
 while True:
-        inp = input("Enter command: ")
+    inp = input("Enter command: ")
     if inp in globals:
-        print("correct")
-        print(line)
+        print("\n")
         globals[inp]()
+        print(line)
     else:
         print("ERROR - invalid command")
-
-#main program
-#getinput() = fundstart
